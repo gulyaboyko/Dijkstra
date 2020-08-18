b990c6e9c88beca0e434d692bc571347b0703da3
Gulya Akhtyamova <gulya.nadym@gmail.com>
2020-08-18 12:20:00 +0300

    Initial Commit

---
+++ GraphShortestWay/GraphShortestWay.h
@@ -0,0 +1,19 @@
+//
+//  GraphShortestWay.h
+//  GraphShortestWay
+//
+//  Created by Gulya Boiko on 8/18/20.
+//  Copyright © 2020 com.gulya.boiko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GraphShortestWay.
+FOUNDATION_EXPORT double GraphShortestWayVersionNumber;
+
+//! Project version string for GraphShortestWay.
+FOUNDATION_EXPORT const unsigned char GraphShortestWayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraphShortestWay/PublicHeader.h>
+
+
